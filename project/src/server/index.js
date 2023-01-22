08e6f28efaeddab9a6a4f207cfa7004540ef67dd
Joseph <Y.M.Taj99@gmail.com>
2023-01-23 00:12:13 +0300

    Server completed

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
+const { Map } = require('immutable')
 
 const app = express()
 const port = 3000
@@ -20,6 +21,25 @@ app.get('/apod', async (req, res) => {
         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
         res.send({ image })
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+app.get('/rovers/:name', async (req, res) => {
+
+    const rover = req.params.name;
+
+    try {
+        let manifest = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+
+        const max_sol = manifest.photo_manifest.max_sol;
+        let images = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${max_sol}&api_key=${process.env.API_KEY}`)
+            .then(res => res.json());
+
+        res.status(200).send(Map(images));
+
     } catch (err) {
         console.log('error:', err);
     }
